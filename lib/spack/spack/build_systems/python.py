f4b21ddfa94a1050c39f260066726e356374259d
Adam J. Stewart <ajstewart426@gmail.com>
2021-12-06 10:36:21 -0600

    Use PythonPackage for installing wheels

--- lib/spack/spack/build_systems/python.py
+++ lib/spack/spack/build_systems/python.py
@@ -73,8 +73,6 @@ class PythonPackage(PackageBase):
             '--no-warn-script-location',
             # Ignore the PyPI package index
             '--no-index',
-            # Install source code in the current directory
-            '.'
         ]
 
     @property
@@ -169,6 +167,11 @@ class PythonPackage(PackageBase):
             args.append('--install-option=' + option)
         for option in self.global_options(spec, prefix):
             args.append('--global-option=' + option)
+
+        if self.stage.archive_file.endswith('.whl'):
+            args.append(self.stage.archive_file)
+        else:
+            args.append('.')
 
         pip = inspect.getmodule(self).pip
         with working_dir(self.build_directory):
